b8fb462d1d51bc3462fb22b07cc153f247d901ab
Raju at LHG <raju@lhgraphics.com>
2023-06-17 03:25:19 +0600

    Updated UI Fro home page and Logins

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    SitemapGenerator::generate_sitemap('https://www.lhgraphics.com/');
     return view('welcome');
 });
 
