8331e6975c43614b0845c1e8575da581761455b3
Raju at LHG <raju@lhgraphics.com>
2023-11-21 23:55:25 +0600

    Updated the cors

--- laravel/app/Http/Middleware/CorsMiddleware.php
+++ laravel/app/Http/Middleware/CorsMiddleware.php
@@ -15,21 +15,8 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Allow from any origin
-        if (isset($request->server()['HTTP_ORIGIN'])) {
-            $origin = $request->server()['HTTP_ORIGIN'];
-
-            header('Access-Control-Allow-Origin: ' . $origin);
-            header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-            header('Access-Control-Allow-Credentials: true');
-        }
-
-        // Handle preflight requests
-        if ($request->getMethod() === 'OPTIONS') {
-            return response('', 200);
-        }
-
-        return $next($request);
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
